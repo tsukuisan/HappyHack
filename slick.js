d427d476ac502a56cfed505f124c37dbf650ec02
津久井千佳 <chika@tsukuichikanoMacBook-Pro.local>
2020-10-02 23:50:56 +0900

    レスポンシブ！

--- slick.js
+++ slick.js
@@ -1,7 +1,33 @@
-$('.slider').slick({
-    autoplay:false,
-    autoplaySpeed:5000,
-    dots:true,
-    slidesToShow:4,
-    slidesToScroll:1
+//$('.slider').slick({
+//    autoplay: false,
+//    autoplaySpeed: 5000,
+//    dots: true,
+//    slidesToShow: 4,
+//    slidesToScroll: 1
+//});
+
+$(window).on('load resize', function () {
+    var winW = $(window).width();
+    var devW = 767;
+    if (winW <= devW) {
+        //767px以下の時の処理
+        $('.slider').slick({
+            autoplay: false,
+            autoplaySpeed: 5000,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    } 
+    
+    else {
+        //768pxより大きい時の処理
+        $('.slider').slick({
+            autoplay: false,
+            autoplaySpeed: 5000,
+            dots: true,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        });
+    }
 });
